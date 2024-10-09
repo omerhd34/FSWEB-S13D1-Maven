0eed8f44518fa1e45205166800c8a00460f5848c
omerhalisd1 <demirome123@gmail.com>
2024-10-10 00:23:16 +0300

    dafa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        return true;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
